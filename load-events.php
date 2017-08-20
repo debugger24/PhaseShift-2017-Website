136655f18ccb99b68aedf65ae5197f5999cfa7d2
Shravan Kumar <kum_shravan@yahoo.com>
2017-08-20 21:19:02 +0530

    Fixed tablet issues.

--- load-events.php
+++ load-events.php
@@ -25,7 +25,7 @@
       }
 
       // Creating a card.
-      echo    "<div class='col s12 m6 l3'>
+      echo    "<div class='col s12 l3'>
                 <div class='card blue-grey darken-1'>
                   <div class='card-content white-text'>";
 
